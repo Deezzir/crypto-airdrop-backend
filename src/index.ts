567c13faf5214171a9dd611556c19aade6157723
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-27 16:13:53 -0400

    1

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ import http from 'http';
 import cors from 'cors';
 import 'dotenv/config';
 import bodyParser from 'body-parser';
-import cookieParser from "cookie-parser";
-import session from "express-session";
-import db from "./db.js";
-import dropRouter from "./router/drop-router.js";
-import checkRouter from "./router/check-router.js";
-import ErrorMiddleware from "./middlewares/error-middleware.js";
-import helmet from "helmet";
-import RateLimiterMiddleware from "./middlewares/ratelimit-middleware.js";
-import dotenv from "dotenv";
-import XApiMiddleware from "./middlewares/xapi-middleware.js";
-import * as common from "./common.js";
-import xService from "./services/x-service.js";
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import db from './db.js';
+import dropRouter from './router/drop-router.js';
+import checkRouter from './router/check-router.js';
+import ErrorMiddleware from './middlewares/error-middleware.js';
+import helmet from 'helmet';
+import RateLimiterMiddleware from './middlewares/ratelimit-middleware.js';
+import dotenv from 'dotenv';
+import XApiMiddleware from './middlewares/xapi-middleware.js';
+import * as common from './common.js';
+import xService from './services/x-service.js';
 dotenv.config();
 
 const mongo = db;
@@ -25,9 +25,9 @@ const httpServer = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 var corsOptions = {
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 // airdropuserService.getAllUsers().then((data) => {
@@ -35,17 +35,17 @@ var corsOptions = {
 
 //   fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
 // });
-
+app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(helmet());
 app.use(
-    session({
-        name: 'session',
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-        cookie: { secure: true },
-    }),
+  session({
+    name: 'session',
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  }),
 );
 
 // const bot = new TelegramBot(process.env.TELEGRAM_BOT, { polling: true });
@@ -56,25 +56,20 @@ app.use(
 //     }
 // });
 
-app.use("/check", checkRouter);
+app.use('/check', checkRouter);
 
 app.use(ErrorMiddleware);
 app.use(RateLimiterMiddleware);
 app.use(XApiMiddleware);
-app.use(cors(corsOptions));
-app.use(
-    '/drop',
-    cookieParser(),
-    bodyParser.json(),
-    dropRouter,
-);
+
+app.use('/drop', cookieParser(), bodyParser.json(), dropRouter);
 
 app.use((req: any, res: any, next: any) => {
-    res.status(404).send({
-        status: 404,
-        error: 'Not Found',
-        message: 'The requested resource was not found on this server.',
-    });
+  res.status(404).send({
+    status: 404,
+    error: 'Not Found',
+    message: 'The requested resource was not found on this server.',
+  });
 });
 
 await new Promise((resolve: any) => httpServer.listen({ port: port }, resolve));
