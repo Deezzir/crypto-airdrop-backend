4ea000efa503ee8a1284ac8ad85d9cb2bd15e716
Iurii Kondrakov <55366304+Deezzir@users.noreply.github.com>
2024-05-27 16:39:02 -0400

    Update

--- src/services/x-service.ts
+++ src/services/x-service.ts
@@ -78,7 +78,7 @@ class XService {
             if (user.follower_count < TWITTER_FOLLOWERS || common.getAgeInDays(user.creation_date) < TWITTER_AGE)
                 return {
                     isValid: false,
-                    errorMsg: 'Account must be Blue verified. Or have at least 30 followers and be at least 60 days old',
+                    errorMsg: `Account must be Blue verified. Or have at least ${TWITTER_FOLLOWERS} followers and be at least ${TWITTER_AGE} days old`,
                 };
         }
 
