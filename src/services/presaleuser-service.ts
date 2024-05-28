bf7e10692933acfe875a6d415bb574e480a71d7a
Iurii Kondrakov <55366304+Deezzir@users.noreply.github.com>
2024-05-28 15:03:52 -0400

    Update presale verif

--- src/services/presaleuser-service.ts
+++ src/services/presaleuser-service.ts
@@ -1,17 +1,24 @@
 import PresaleUserModel from "../models/presaleUser.js";
 import { PresaleUser } from "../common.js";
+import { Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const PRESALE_MAX_SOL_AMOUNT = parseFloat(process.env.PRESALE_MAX_SOL_AMOUNT) || 0;
+const PRESALE_MIN_SOL_AMOUNT = parseFloat(process.env.PRESALE_MIN_SOL_AMOUNT) || 0;
+const RPC_URL = new Connection(process.env.RPC_URL || 'https://api.devnet.solana.com', 'confirmed');
+const DROP_PUBKEY = process.env.DROP_PUBKEY || '';
 
 class PresaleUserService {
     async verify(user: PresaleUser): Promise<{ isValid: boolean, errorMsg: string | undefined }> {
+        const isValidTx = await this.verifySignature(user.txEnroll);
+        if (!isValidTx.isValid) return isValidTx;
+
         const record = await this.getUserByWallet(user.wallet);
         if (!record) return { isValid: true, errorMsg: undefined };
 
         if (record.solAmount + user.solAmount > PRESALE_MAX_SOL_AMOUNT) {
-            return { isValid: false, errorMsg: `You will exceed the maximum amount of ${PRESALE_MAX_SOL_AMOUNT} SOL` };
+            return { isValid: false, errorMsg: `You exceeded the maximum amount of ${PRESALE_MAX_SOL_AMOUNT} SOL. Contact dev for refund` };
         }
 
         return { isValid: true, errorMsg: undefined };
@@ -56,6 +63,32 @@ class PresaleUserService {
         userFound.txEnroll.push(user.txEnroll);
 
         return await userFound.save();
+    }
+
+    async verifySignature(signature: string): Promise<{ isValid: boolean, errorMsg: string | undefined }> {
+        const { value: status } = await RPC_URL.getSignatureStatus(signature);
+
+        if (status && ((status.confirmationStatus === 'confirmed' || status.confirmationStatus === 'finalized')) && status.err === null) {
+            const details = await RPC_URL.getTransaction("3uVuXFfFTqQw5jRi8Cpvpa6Jt578ASVYMnDrE3xFAdf4voStYbfMU7rq8sc8JmwfadVAXJUeNZ1M2o4iSVzdnJ1u", {
+                commitment: 'confirmed',
+                maxSupportedTransactionVersion: 0
+            });
+
+            const balance_change = (details?.meta?.postBalances[1] ?? 0) - (details?.meta?.preBalances[1] ?? 0) / LAMPORTS_PER_SOL;
+            const receiver = details?.transaction.message.getAccountKeys().get(1)?.toString() ?? '';
+
+            if (receiver !== DROP_PUBKEY) {
+                return { isValid: false, errorMsg: 'Invalid receiver' };
+            }
+
+            if (balance_change < (PRESALE_MIN_SOL_AMOUNT - 0.0001) || balance_change > (PRESALE_MAX_SOL_AMOUNT + 0.0001)) {
+                return { isValid: false, errorMsg: 'Invalid amount' };
+            }
+
+            return { isValid: true, errorMsg: undefined };
+        }
+
+        return { isValid: false, errorMsg: 'Transaction failed' }
     }
 }
 
